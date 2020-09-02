74397e910f48cf58ea3b61d4fe91f5ca42bd684c
Federico Knispel <knispel.federico@gmail.com>
2020-09-02 19:42:55 -0300

    more logs

--- app.js
+++ app.js
@@ -26,33 +26,47 @@ const isProd = process.env.NODE_ENV == 'prod';
 let hasAnyError = '';
 // Whatsapp session
 if (isProd) {
-
     console.log('Estamos en prod');
+    hasAnyError = hasAnyError + `
+    Modo Prod:`;
     sessionEnv = {
         WABrowserId: process.env.WABrowserId,
         WASecretBundle: process.env.WASecretBundle,
         WAToken1: process.env.WAToken1,
         WAToken2: process.env.WAToken2
     }
-
+    hasAnyError = hasAnyError + `
+    ${sessionEnv.WABrowserId}`;
     //sessionEnv = JSON.stringify(sessionEnv);
 
 } else {
 
     console.log('Estamos en Dev');
+    hasAnyError = hasAnyError + `
+    Modo Dev`;
     SESSION_FILE_PATH = './session.json';
 
     if (fs.existsSync(SESSION_FILE_PATH)) {
+        hasAnyError = hasAnyError + `
+        Existe un archivo Json`;
         sessionJson = require(SESSION_FILE_PATH);
     } else {
-        hasAnyError = hasAnyError + 'NO EXISTE EL ARCHIVO'
+        hasAnyError = hasAnyError + `
+        No existe dicho archivo`;
         headless = false;
     }
 }
 
+hasAnyError = hasAnyError + `
+Vamos a instanciar el cliente`;
 const client = new Client({ puppeteer: { headless }, session: isProd ? sessionEnv : sessionJson });
 
+hasAnyError = hasAnyError + `
+Teoricamente existe = ${!!client}`;
 client.initialize()
+
+hasAnyError = hasAnyError + `
+Teoricamente inicializamos el cliente`;
 
 client.on('qr', (qr) => {
     // NOTE: This event will not be fired if a session is specified.
@@ -83,21 +97,23 @@ client.on('auth_failure', msg => {
 
 client.on('ready', () => {
     console.log('Bot has been a wake-up');
+    hasAnyError = hasAnyError + `
+    Bot desperto`;
 });
 
 client.on('message', driverMsg);
 
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
 
-app.get('/logs', function (req, res) {
-    res.send(`Is Prod: ${isProd}
-    ${isProd, JSON.stringify(sessionEnv), JSON.stringify(sessionJson)},
-    ERROR: ${hasAnyError}`)
-})
 
 // Exports
 global.client = client;
+
 module.exports = app;
+
+app.get('/', function (req, res) {
+    res.send(`Is Prod: ${isProd}
+    ${JSON.stringify(sessionEnv)}
+    ${JSON.stringify(sessionJson)}
+    LOGS: ${hasAnyError}`)
+})
